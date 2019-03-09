f73028306cf9565322d26ff43382a553b26d1d8a
Terrasse <3305049949@qq.com>
2019-03-09 01:35:15 +0000

    f

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 #define MAX 1000000000
 int main()
 {
-	for(int i=1;i<=MAX;i++);
+	for(register int i=1;i<=MAX;i++);
 	return 0;
 }
--- mian.cpp
+++ mian.cpp
@@ -1,6 +1,6 @@
 #define MAX 1000000000
 int main()
 {
-	for(int i[1]={1};i[0]<=MAX;i[0]++);
+	for(int i[30000]={1};i[29999]<=MAX;i[29999]++);
 	return 0;
 }
